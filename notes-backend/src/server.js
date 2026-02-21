34bf9055b72165be739b5df55676e4bd5b93554c
Nikesh Gyawali <gnikesh03@gmail.com>
2026-02-21 13:37:11 -0600

    feat: update apis and routes for notes app

--- notes-backend/src/server.js
+++ notes-backend/src/server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { noteRouter } = require('./routes');
 const app = express();
 
-app.get('/api/notes', (req, res) => res.json({ message: 'Hello from notes!'}));
+app.use(bodyParser.json());
+app.use('/api/notes', noteRouter);
 
 port = process.env.PORT;
 
-mongoose.connect(process.env.DB_URL).then(() => {
-    console.log('Connected to MongoDB! Starting server.');
-    app.listen(port, () => {
-        console.log(`Notes server listening on port ${port}`);
+mongoose
+    .connect(process.env.DB_URL)
+    .then(() => {
+        console.log('Connected to MongoDB! Starting server.');
+        app.listen(port, () => {
+            console.log(`Notes server listening on port ${port}`);
+        });
+    })
+    .catch(err => {
+        console.error("Something went wrong!");
+        console.error(err);
     });
-}).catch(err => {
-    console.error("Something went wrong!");
-    console.error(err);
-})
 
 
 
