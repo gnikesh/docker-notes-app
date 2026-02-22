1f8f33cdbe7e9b467aa93fe5a1ed5a234c3d72c7
Nikesh Gyawali <gnikesh03@gmail.com>
2026-02-22 12:00:50 -0600

    feat: add api supports to notes backend

--- notes-backend/src/routes.js
+++ notes-backend/src/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const axios = require('axios');
 const { Note } = require('./models');
+
 const noteRouter = express.Router();
+const notebooksApiUrl = process.env.NOTBOOKS_API_URL;
 
 const validateId = (req, res, next) => {
     const { id } = req.params;
@@ -24,8 +27,37 @@ noteRouter.post('/', async (req, res, next) => {
         if (!title || !content) {
             return res.status(400).json({ error: "'title', 'content' fields are required" });
         }
+        
+        let validatedNotebookId = null;
 
-        const note = new Note({ title, content });
+        if (!notebookId) {
+            console.info({
+                message: "Notebook id not provided. Storing note without notebook association.",
+            });
+        } else if (!mongoose.Types.ObjectId.isValid(notebookId)) {
+            return res.status(400).json({ error: "Notebook not found.", notebookId });
+        } else {
+            try {
+                await axios.get(`${notebooksApiUrl}/${notebookId}`);    
+                validatedNotebookId = notebookId;
+
+            } catch (err) {
+                const jsonError = err.toJSON();
+                if (jsonError.status === 404) {
+                    return res.status(400).json({ error: "Notebook not found.", notebookId });
+                } else {
+                    console.error({
+                        message: "Error verifying the Notebook ID. Upstream Notebooks service not available. Storing note with provided id for later verification.", notebookId, 
+                        error: err.message,
+                    });
+                    // post something in a queue for later processing.
+                }
+            } finally {
+                validatedNotebookId = notebookId;
+            }
+        }
+
+        const note = new Note({ title, content, notebookId: validatedNotebookId });
         await note.save();
         res.status(201).json( {data: note} );
         
